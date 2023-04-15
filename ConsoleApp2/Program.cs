efcf50c66d0eeb8aa884031083002a95f5c50cea
zahrajafari <jafari_zhr@yahoo.com>
2023-04-15 20:46:09 +0330

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class KmmBmm
+    {
+        static void Main()
+        {
+            Console.Write("Please enter num 1:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter num 2:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("KMM = " + Kmm(num1, num2));
+            Console.WriteLine("BMM = " + Bmm(num1, num2));
+        }
+
+        public static int Kmm(int a, int b)
+        {
+            int max;
+            if (a > b)
+                max = a;
+            else
+                max = b;
+
+            while (true)
+            {
+                if (max % a == 0 && max % b == 0)
+                    break;
+                else
+                    ++max;
+            }
+            return max;
+        }
+
+        public static int Bmm(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+            return a;
+        }
+    }
+}
